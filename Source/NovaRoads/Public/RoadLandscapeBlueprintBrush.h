6ea6ae0802daa4b86aa419326cbae9451515a7f0
Kamalpreet <kpsngamer4@gmail.com>
2025-07-09 21:42:33 +0530

    RoadLandscapeBlueprintBrush

---
+++ Source/NovaRoads/Public/RoadLandscapeBlueprintBrush.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LandscapeBlueprintBrush.h"
+#include "RoadLandscapeBlueprintBrush.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOVAROADS_API ARoadLandscapeBlueprintBrush : public ALandscapeBlueprintBrush
+{
+	GENERATED_BODY()
+	
+};
